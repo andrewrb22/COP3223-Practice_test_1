9340f80ba0308dcc0e20449d604b8c5bc10a0acd
Andrew Rodriguez <andrewrb22@gmail.com>
2022-02-15 18:32:02 -0500

    First questions and answers(testing each)

--- PracticeTest1.c
+++ PracticeTest1.c
@@ -3,7 +3,25 @@
 
 int main(){
 
-printf("First commit");
 
     return 0;
 }
+
+// Question # 1
+// int x = 1, y = 2 , z = 3;
+// printf("x is %d", z);
+// x =y;
+//Answer: x is 3
+//*****************************//
+// Question # 2
+// int x =1, y = 2, z = 3;
+// x = y;
+// printf("x is %d", (z+y-1)/x+x);
+//Answer: x = 4 (None of the above)
+//******************************//
+// Question # 3
+// int x = 1;
+// ++x;
+// printf("x++ is %d", x);
+//Answer: x++ is 2 
+//******************************//
